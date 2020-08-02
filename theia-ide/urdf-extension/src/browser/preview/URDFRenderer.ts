1bc8d69d3cfaffc1e5e79b3a04fea74a5332dd1f
Wolfgang <mailtogeck@gmail.com>
2020-08-02 14:25:45 +0200

    fixed continuous joints

--- theia-ide/urdf-extension/src/browser/preview/URDFRenderer.ts
+++ theia-ide/urdf-extension/src/browser/preview/URDFRenderer.ts
@@ -1,8 +1,8 @@
 import * as BABYLON from '@babylonjs/core/Legacy/legacy';
 import '@babylonjs/loaders';
-import {AdvancedDynamicTexture, Control, Slider, StackPanel, TextBlock} from '@babylonjs/gui/2D';
-import {AbstractMesh, ArcRotateCamera, TransformNode, Material } from '@babylonjs/core/Legacy/legacy';
-import {BaseModel, JointInfo, RobotDescription} from './UrdfModel';
+import { AdvancedDynamicTexture, Control, Slider, StackPanel, TextBlock } from '@babylonjs/gui/2D';
+import { AbstractMesh, ArcRotateCamera, TransformNode, Material } from '@babylonjs/core/Legacy/legacy';
+import { BaseModel, JointInfo, RobotDescription } from './UrdfModel';
 
 
 /**
@@ -10,9 +10,9 @@ import {BaseModel, JointInfo, RobotDescription} from './UrdfModel';
  */
 export class URDFRenderer {
   private scene: BABYLON.Scene;
-  private idMeshMap = new Map<string,AbstractMesh>();
-  private idMaterialMap = new Map<string,Material>();
-  
+  private idMeshMap = new Map<string, AbstractMesh>();
+  private idMaterialMap = new Map<string, Material>();
+
   private menuPanel: StackPanel;
 
   public attachCanvas(canvas: HTMLCanvasElement) {
@@ -23,7 +23,7 @@ export class URDFRenderer {
   private createScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
     this.scene = new BABYLON.Scene(engine);
     this.scene.clearColor = BABYLON.Color4.FromInts(0, 0, 0, 0);
-    
+
     const camera = new BABYLON.ArcRotateCamera('camera', 0, Math.PI / 4, 3, BABYLON.Vector3.Zero(), this.scene);
     camera.wheelPrecision = 100;
     camera.lowerRadiusLimit = 2;
@@ -37,7 +37,7 @@ export class URDFRenderer {
     this.menuPanel.width = "120px";
     this.menuPanel.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_RIGHT;
     this.menuPanel.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
-    this.menuPanel.paddingRight=5;
+    this.menuPanel.paddingRight = 5;
     menuTexture.addControl(this.menuPanel);
 
     engine.runRenderLoop(() => {
@@ -51,24 +51,22 @@ export class URDFRenderer {
   }
 
   public resetModel() {
-    this.idMeshMap.forEach( value => value.dispose());
-    this.idMaterialMap.forEach(value=> value.dispose());    
+    this.idMeshMap.forEach(mesh => { mesh.material?.dispose(); mesh.dispose(); });
     this.menuPanel.clearControls();
   }
 
   highlightMeshes(ids: string[]) {
-    
-    this.idMeshMap.forEach( (mesh:AbstractMesh,id:string) => {
-        if(mesh.material){
-          mesh.material.alpha= ids.length==0 || ids.includes(id)  ? 1.0 :0.2;
-        }
-    });    
+    this.idMeshMap.forEach((mesh: AbstractMesh, id: string) => {
+      if (mesh.material) {
+        mesh.material.alpha = ids.length == 0 || ids.includes(id) ? 1.0 : 0.4;
+      }
+    });
   }
 
   public resetView() {
-    const camera=this.scene.getCameraByID("camera") as ArcRotateCamera;
-    camera.alpha=0;
-    camera.beta=Math.PI / 4;
+    const camera = this.scene.getCameraByID("camera") as ArcRotateCamera;
+    camera.alpha = 0;
+    camera.beta = Math.PI / 4;
     camera.setTarget(BABYLON.Vector3.Zero());
   }
 
@@ -77,7 +75,8 @@ export class URDFRenderer {
     this.setupBoxes(robot);
     this.setupCylinders(robot);
     this.setupSpheres(robot);
-    this.setupMeshes(robot);        
+    this.setupMeshes(robot);
+    this.idMaterialMap.forEach(material => material.dispose());
   }
 
   private setupCylinders(robot: RobotDescription) {
@@ -115,8 +114,7 @@ export class URDFRenderer {
   }
 
   private setupMeshes(robot: RobotDescription) {
-    
-    const assetsManager = new BABYLON.AssetsManager(this.scene);    
+    const assetsManager = new BABYLON.AssetsManager(this.scene);
     for (const meshVisual of robot.meshVisuals) {
 
       const rootUrl = encodeURI(meshVisual.fileName) + '&sceneName=';
@@ -128,7 +126,7 @@ export class URDFRenderer {
           this.setupBaseProperties(loadedMesh, meshVisual);
           loadedMesh.scaling = new BABYLON.Vector3(this.getScaleFactor(meshVisual.scaleX), this.getScaleFactor(meshVisual.scaleY), this.getScaleFactor(meshVisual.scaleZ));
         }
-      };      
+      };
     }
 
     assetsManager.onFinish = tasks => {
@@ -149,7 +147,7 @@ export class URDFRenderer {
       colorMaterial.alpha = color.alpha;
       colorMaterial.zOffset = robot.colors.indexOf(color);
       this.idMaterialMap.set(color.id, colorMaterial);
-      
+
     }
     for (const texture of robot.textures) {
       const textureMaterial = new BABYLON.StandardMaterial(texture.id, this.scene);
@@ -162,7 +160,7 @@ export class URDFRenderer {
   private setupBaseProperties(mesh: AbstractMesh, model: BaseModel) {
     mesh.position = new BABYLON.Vector3(model.x, model.y, model.z);
     mesh.rotation = new BABYLON.Vector3(model.roll, model.pitch, model.yaw);
-    mesh.material = this.idMaterialMap.get(model.materialId)!.clone(mesh.name+"-material");
+    mesh.material = this.idMaterialMap.get(model.materialId)!.clone(mesh.name + "-material");
   }
 
   private connectMeshes(robot: RobotDescription) {
@@ -173,6 +171,13 @@ export class URDFRenderer {
       transform.rotation = new BABYLON.Vector3(joint.roll, joint.pitch, joint.yaw);
 
       if (joint.type == 'revolute') {
+        this.addControlForRevoluteJoint(joint, transform);
+      }
+
+      if (joint.type == 'continuous') {
+        //not really continous but full clock- and counterclockwise rotation 
+        joint.lowerLimit = -Math.PI * 2;
+        joint.upperLimit = Math.PI * 2;
         this.addControlForRevoluteJoint(joint, transform);
       }
 
@@ -187,36 +192,36 @@ export class URDFRenderer {
 
 
   private addControlForRevoluteJoint(joint: JointInfo, transform: TransformNode) {
-    
-    if(joint.upperLimit ==0 && joint.lowerLimit == 0){
+
+    if (joint.upperLimit == 0 && joint.lowerLimit == 0) {
       return
     }
 
     const header = new TextBlock();
-    this.setHeaderText(header, joint.id,0);
+    this.setHeaderText(header, joint.id, 0);
     header.height = "20px";
-    header.paddingTop=2;
+    header.paddingTop = 2;
     header.fontSize = 10;
     header.color = "grey";
 
     const slider = new Slider();
     slider.minimum = joint.lowerLimit;
     slider.maximum = joint.upperLimit;
-    slider.value = (joint.upperLimit + joint.lowerLimit)/2;
+    slider.value = (joint.upperLimit + joint.lowerLimit) / 2;
     slider.height = "12px";
-    slider.color = "green";  
+    slider.color = "green";
 
     slider.onValueChangedObservable.add(value => {
       transform.rotation = new BABYLON.Vector3(joint.roll, joint.pitch, joint.yaw);
       transform.rotate(new BABYLON.Vector3(joint.axeX, joint.axeY, joint.axeZ), value, BABYLON.Space.LOCAL);
-      this.setHeaderText(header, joint.id,value);
+      this.setHeaderText(header, joint.id, value);
     });
 
     this.menuPanel.addControl(header);
     this.menuPanel.addControl(slider);
   }
 
-  private setHeaderText(header: TextBlock, name:String,value:number ){
+  private setHeaderText(header: TextBlock, name: String, value: number) {
     header.text = name + ": " + value.toFixed(2);
   }
 
